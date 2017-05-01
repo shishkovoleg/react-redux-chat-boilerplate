4598d826565999fc291ad3789deff91ec30915f1
shishkovoleg <shishkovoleg@gmail.com>
2017-05-01 18:54:27 +0300

    Add boilerplate twitter bootstrap navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import Contacts from '../containers/Contacts';
 import Chat from '../components/Chat';
+import Header from '../components/Header';
 
 class App extends Component {
   render() {
     return (
 		<div className="container">
+			<Header/>
 			<Contacts contactList={this.props.contactList}/>
 			<Chat/>
 		</div>
